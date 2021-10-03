cb10031f234b18199d2510a4ea9c19a0379b3b39
WingCH <wingch.apps@gmail.com>
2021-10-03 20:11:35 +0800

    add gitignore

--- Pods/Target Support Files/Pods-NFCReader/Pods-NFCReader-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NFCReader : NSObject
-@end
-@implementation PodsDummy_Pods_NFCReader
-@end
--- Pods/Target Support Files/TRETJapanNFCReader/TRETJapanNFCReader-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TRETJapanNFCReader : NSObject
-@end
-@implementation PodsDummy_TRETJapanNFCReader
-@end
