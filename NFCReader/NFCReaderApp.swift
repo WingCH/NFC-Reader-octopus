02265bf66f8038ff6dce9defdee2dd6c582f2399
WingCH <wingch.apps@gmail.com>
2021-10-03 15:37:34 +0800

    Initial Commit

---
+++ NFCReader/NFCReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCReaderApp.swift
+//  NFCReader
+//
+//  Created by WingCH on 3/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NFCReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
